26fb0589dd8c12cc0293cbd543373fed755ca2e4
k_yoshida <k_yoshida@thinkup-sol.co.jp>
2016-01-12 10:50:50 +0900

    社員管理画面 検索処理追加

--- MembersListManagementProgram/MembersMasterEditForm.cs
+++ MembersListManagementProgram/MembersMasterEditForm.cs
@@ -16,11 +16,19 @@ namespace MembersListManagementProgram
     public partial class MembersMasterEditForm : Form
     {
         private string editMode { get; set; }
+        private string primaryKey1 { get; set; }
+        private string primaryKey2 { get; set; }
+
         public MembersMasterEditForm(string editMode, params string[] args)
         {
             InitializeComponent();
             this.editMode = editMode;
             this.Text = getFormTitle();
+            if (args.Count() == 2)
+            {
+                this.primaryKey1 = args[0];
+                this.primaryKey2 = args[1];
+            }
         }
 
 
@@ -28,6 +36,8 @@ namespace MembersListManagementProgram
         {
             // ボタン表示・非表示切り替え
             switchVisibleButton();
+            // TODO: 編集、参照時データ取得処理実装
+            if (!this.editMode.Equals(CommonConstants.CREATE_MODE)) excuteSearch();
         }
 
         // タイトル取得
@@ -85,6 +95,52 @@ namespace MembersListManagementProgram
             excuteSql(getSqlString());
         }
 
+
+        // 検索
+        private void excuteSearch()
+        {
+            OleDbDataReader dRead;
+            OleDbCommand com;
+            OleDbConnection cn = new OleDbConnection();
+            // 接続文字列を設定して接続する
+            cn.ConnectionString = ConfigurationManager.ConnectionStrings["MembersListManagementProgram.Properties.Settings.ConnectionString"].ConnectionString;
+            try
+            {
+                cn.Open();
+                string sql = "SELECT CD_CO, CD_EMP, NM_EMP, TXT_PASSWD, CD_DEPT, TXT_ZIP, TXT_ADDR1, TXT_ADDR2, TXT_ADDR3, TXT_TEL, TXT_FAX, TXT_REM FROM M_EMP WHERE CD_CO='{0}' AND CD_EMP='{1}'";
+                com = new OleDbCommand(String.Format(sql, primaryKey1, primaryKey2), cn);
+                dRead = com.ExecuteReader();
+
+                while (dRead.Read())
+                {
+                    this.textBox1.Text = dRead.GetString(0);
+                    this.textBox2.Text = dRead.GetString(1);
+                    this.textBox3.Text = dRead.GetString(2);
+                    this.textBox4.Text = dRead.GetString(3);
+                    this.textBox5.Text = dRead.GetString(4);
+                    this.textBox6.Text = dRead.GetString(5);
+                    this.textBox7.Text = dRead.GetString(6);
+                    this.textBox8.Text = dRead.GetString(7);
+                    this.textBox9.Text = dRead.GetString(8);
+                    this.textBox10.Text = dRead.GetString(9);
+                    this.textBox11.Text = dRead.GetString(10);
+                    this.textBox12.Text = dRead.GetString(11);
+                }
+                dRead.Close();
+                cn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "通知");
+            }
+            finally
+            {
+                //if (dRead != null) dRead.Close();
+                if (cn != null) cn.Close();
+            }
+        }
+
+        // SQL実行
         private void excuteSql(string sql)
         {
             OleDbConnection cn = new OleDbConnection();
@@ -138,7 +194,7 @@ namespace MembersListManagementProgram
                 // TODO: 
                 sql = String.Format(
                     sql, textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text, textBox5.Text, textBox6.Text,
-                    textBox7.Text, textBox8.Text, textBox9.Text, textBox10.Text, textBox11.Text, textBox12.Text, "k_yoshida", "1", "1");
+                    textBox7.Text, textBox8.Text, textBox9.Text, textBox10.Text, textBox11.Text, textBox12.Text, "k_yoshida", primaryKey1, primaryKey2);
             }
             return sql;
         }
@@ -146,7 +202,7 @@ namespace MembersListManagementProgram
         // 削除
         private void deleteButton_Click(object sender, EventArgs e)
         {
-            excuteSql(String.Format("UPDATE M_EMP DTM_UPDATE=SYSDATE, SET FLG_ACTIVE='N' WHERE CD_CO='{0}' AND CD_EMP='{1}'", "1", "1"));
+            excuteSql(String.Format("UPDATE M_EMP SET DTM_UPDATE=SYSDATE, FLG_ACTIVE='N' WHERE CD_CO='{0}' AND CD_EMP='{1}'", primaryKey1, primaryKey2));
         }
 
         // 終了
